544ebdb933d15bbdc06a59838fed288b649a7ebf
Domingos <domingos.adj@gmail.com>
2026-01-15 11:59:55 -0300

    api draft

--- main.go
+++ main.go
@@ -3,14 +3,60 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"encoding/json"
+	"strings"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "pong")
+}
+
+func shortenHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	type Payload struct {
+		URL string `json:"url"`
+	}
+
+	var pay Payload
+	if err := json.NewDecoder(r.Body).Decode(&pay); err != nil || pay.URL == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	type Response struct{
+		Short string `json:"short_url"`
+		Original string `json:"original_url"`
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type","application/json")
+	
+	json.NewEncoder(w).Encode(Response{
+		Short: "asd",
+		Original: pay.URL,
+	})
+	
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	code := strings.TrimPrefix(r.URL.Path, "/")
+	if code == "" || code == "shorten" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	url := "https://google.com"
+
+	http.Redirect(w, r, url, http.StatusFound)
 }
 
 func main() {
-	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/ping", pingHandler)
+	http.HandleFunc("/shortener", shortenHandler)
 
 	fmt.Println("Servidor rodando em http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
--- main_test.go
+++ main_test.go
@@ -6,17 +6,17 @@ import (
 	"testing"
 )
 
-func TestHelloHandler(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, "/hello", nil)
+func TestPingHandler(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/ping", nil)
 	res := httptest.NewRecorder()
 
-	helloHandler(res, req)
+	pingHandler(res, req)
 
 	if res.Code != http.StatusOK {
 		t.Fatalf("esperado status 200, recebido %d", res.Code)
 	}
 
-	expected := "Hello, World!\n"
+	expected := "pong\n"
 	if res.Body.String() != expected {
 		t.Fatalf("esperado %q, recebido %q", expected, res.Body.String())
 	}
